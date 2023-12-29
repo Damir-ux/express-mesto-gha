fa46e4b6315871859c51661e71008d7af405dcd3
Irmad <monte-carlo1991@yandex.ru>
2023-12-29 12:18:47 +0300

    Проектная работа 13 на проверку

---
+++ routes/cards.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const {
+  addCard, getCards, deleteCard, likeCard, dislikeCard,
+
+} = require('../controllers/cards');
+
+router.get('/', getCards);
+router.delete('/:cardid', deleteCard);
+router.post('/', addCard);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
+
+module.exports = router;
