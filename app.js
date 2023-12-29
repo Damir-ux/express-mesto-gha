367c92ebe3c040ec0c3a63bfc22d7766eef189de
Irmad <monte-carlo1991@yandex.ru>
2023-12-29 13:22:08 +0300

    Проектная работа №13 на проверку

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  // console.log('Server is running on port 3000');
 });
 
 app.use('/users', require('./routes/users'));
